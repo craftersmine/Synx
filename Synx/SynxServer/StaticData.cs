e91e33f9edeb6fa4e3b89138e5ce503ab9b28de5
Rodion <rodion.morozov.01@gmail.com>
2018-04-17 23:52:51 +0700

    Added class with static properties

---
+++ Synx/SynxServer/StaticData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+using craftersmine.Packager.Lib.Core;
+using craftersmine.Packager.Lib.Core.Exceptions;
+
+namespace craftersmine.Synx.Server
+{
+    public sealed class StaticData
+    {
+        public static WebSocketServer ServerInstance { get; set; }
+        public static Utils.Logger LoggerInstance { get; set; }
+
+        public static string UserStorage { get; set; }
+        public static string LogsPath { get; set; }
+        public static string ServerRoot { get; set; }
+    }
+}
