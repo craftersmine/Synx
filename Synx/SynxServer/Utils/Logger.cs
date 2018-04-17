34bbca0eb0cb4f3892e26e85c743fe7a0bbd972b
Rodion <rodion.morozov.01@gmail.com>
2018-04-17 23:52:37 +0700

    Added Server Logger

---
+++ Synx/SynxServer/Utils/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Synx.Server.Utils
+{
+    public sealed class Logger
+    {
+        private string _loadTime;
+        private string _file;
+
+        public Logger(string name)
+        {
+            string directory = StaticData.LogsPath;
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+            if (DateTime.Now.Hour.ToString().Length < 2)
+                _loadTime = DateTime.Now.ToShortDateString() + "_0" + DateTime.Now.ToShortTimeString();
+            else _loadTime = DateTime.Now.ToShortDateString() + "_" + DateTime.Now.ToShortTimeString();
+            _file = Path.Combine(directory, name + "_" + _loadTime.Replace(':', '-') + ".log");
+            File.WriteAllText(_file, "");
+        }
+
+        public void Log(string prefix, string contents)
+        {
+            string _date;
+            if (DateTime.Now.Hour.ToString().Length < 2)
+                _date = DateTime.Now.ToShortDateString() + " 0" + DateTime.Now.ToShortTimeString();
+            else _date = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            string logLineCtor = _date + " [" + prefix.ToUpper() + "]" + " " + contents + "\r\n";
+            File.AppendAllText(_file, logLineCtor);
+            Console.Write(logLineCtor);
+        }
+    }
+}
