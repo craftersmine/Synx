562ddee2fef6bc54c16bb0a96dec9fe3bfb6034d
Rodion <rodion.morozov.01@gmail.com>
2018-04-17 23:52:28 +0700

    Added Server

---
+++ Synx/SynxServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+using craftersmine.Packager.Lib.Core;
+using craftersmine.Packager.Lib.Core.Exceptions;
+using craftersmine.Synx.Server.Core;
+using System.IO;
+
+namespace craftersmine.Synx.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticData.ServerRoot = Environment.CurrentDirectory;
+            StaticData.LogsPath = Path.Combine(StaticData.ServerRoot, "logs");
+            StaticData.LoggerInstance = new Utils.Logger("synx-server");
+            Log("info", "Initiating server...");
+            ServerEnvironment.InitiateEnvironment(StaticData.ServerRoot);
+            Console.ReadKey();
+        }
+
+        protected static void Log(string prefix, string contents)
+        {
+            StaticData.LoggerInstance.Log(prefix, contents);
+        }
+    }
+}
---
+++ Synx/SynxServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SynxServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SynxServer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9413c9ca-0e03-4bd0-883e-ba0a62f3a3f6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
