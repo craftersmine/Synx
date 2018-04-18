267b1bafa8a39c853ac2a12737a8f71439db5170
Rodion <rodion.morozov.01@gmail.com>
2018-04-18 23:23:40 +0700

    Improved server config check

--- Synx/SynxServer/Core/ServerEnvironment.cs
+++ Synx/SynxServer/Core/ServerEnvironment.cs
@@ -9,29 +9,61 @@ namespace craftersmine.Synx.Server.Core
 {
     public sealed class ServerEnvironment
     {
+        private static Dictionary<string, string> predefinedConfig = new Dictionary<string, string>();
+
         public static void InitiateEnvironment(string serverRoot)
         {
-            Log("info", "Initiating server environment...");
-            StaticData.UserStorage = Path.Combine(serverRoot, "user-storage");
-            string serverPropsFile = Path.Combine(serverRoot, "server-properties.cfg");
-            string banlistFile = Path.Combine(serverRoot, "banned-accounts.lst");
-            string accountsDataFile = Path.Combine(serverRoot, "accounts-data.lst");
-            if (!Directory.Exists(StaticData.UserStorage))
-                Directory.CreateDirectory(StaticData.UserStorage);
-            if (!File.Exists(serverPropsFile))
+            predefinedConfig.Add("port", "30203");
+            predefinedConfig.Add("bind-ip", "");
+            predefinedConfig.Add("motd", "Synx Server");
+            predefinedConfig.Add("require-auth", "true");
+            predefinedConfig.Add("allow-anonymous", "false");
+
+            try
             {
-                List<string> props = new List<string>();
-                props.Add("port=30203");
-                props.Add("bind-ip=");
-                props.Add("motd=Synx Server");
-                props.Add("require-auth=true");
-                props.Add("allow-anonymous=false");
-                File.WriteAllLines(serverPropsFile, props);
+                Log("info", "Initiating server environment...");
+                StaticData.UserStorage = Path.Combine(serverRoot, "user-storage");
+                string serverPropsFile = Path.Combine(serverRoot, "server-properties.cfg");
+                string banlistFile = Path.Combine(serverRoot, "banned-accounts.lst");
+                string accountsDataFile = Path.Combine(serverRoot, "accounts-data.lst");
+                if (!Directory.Exists(StaticData.UserStorage))
+                    Directory.CreateDirectory(StaticData.UserStorage);
+                if (!File.Exists(serverPropsFile))
+                {
+                    List<string> cfg = new List<string>();
+                    foreach (var predefCfg in predefinedConfig)
+                        cfg.Add(string.Join("=", predefCfg.Key, predefCfg.Value));
+                    File.WriteAllLines(serverPropsFile, cfg);
+                }
+                else
+                {
+                    string[] lines = File.ReadAllLines(serverPropsFile);
+                    List<string> toWrite = new List<string>();
+                    List<string> keys = new List<string>();
+                    foreach (var prop in lines)
+                        keys.Add(prop.Split('=')[0]);
+                    foreach (var key in predefinedConfig)
+                    {
+                        if (!keys.Contains(key.Key))
+                        {
+                            toWrite.Add(string.Join("=", key.Key, key.Value));
+                        }
+                    }
+                    if (toWrite.Any())
+                    {
+                        toWrite.AddRange(lines);
+                        File.WriteAllLines(serverPropsFile, toWrite);
+                    }
+                }
+                if (!File.Exists(banlistFile))
+                    File.WriteAllText(banlistFile, "");
+                if (!File.Exists(accountsDataFile))
+                    File.WriteAllText(accountsDataFile, "");
             }
-            if (!File.Exists(banlistFile))
-                File.WriteAllText(banlistFile, "");
-            if (!File.Exists(accountsDataFile))
-                File.WriteAllText(accountsDataFile, "");
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         private static void Log(string prefix, string contents)
