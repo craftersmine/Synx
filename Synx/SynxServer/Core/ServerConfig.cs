7fabb8e614c3ff80d1ba3690ee1f4232536da6d6
Rodion <rodion.morozov.01@gmail.com>
2018-04-19 22:39:11 +0700

    Added server config reader

---
+++ Synx/SynxServer/Core/ServerConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using craftersmine.Synx.Server.Utils;
+
+namespace craftersmine.Synx.Server.Core
+{
+    public sealed class ServerConfig
+    {
+        private static Dictionary<string, string> cfg = new Dictionary<string, string>();
+
+        public static void LoadConfig()
+        {
+            Program.Log("info", "Loading server configuration...");
+            string[] file = File.ReadAllLines(Path.Combine(StaticData.ServerRoot, "server-properties.cfg"));
+            foreach (var ln in file)
+            {
+                string[] kvp = ln.Split('=');
+                cfg.Add(kvp[0], kvp[1]);
+            }
+        }
+
+        public static string GetString(string key)
+        {
+            string val = "";
+            if (cfg.TryGetValue(key, out val))
+                return val;
+            else throw new KeyNotFoundException("Config key \"" + key + "\" not found in file!");
+        }
+    }
+}
