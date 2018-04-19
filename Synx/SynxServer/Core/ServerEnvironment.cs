a80ca7d52018ebd74c92e7f7478d440025368513
Rodion <rodion.morozov.01@gmail.com>
2018-04-19 22:38:40 +0700

    Removed exception handling

--- Synx/SynxServer/Core/ServerEnvironment.cs
+++ Synx/SynxServer/Core/ServerEnvironment.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using craftersmine.Synx.Server.Utils;
 
 namespace craftersmine.Synx.Server.Core
 {
@@ -19,66 +20,50 @@ namespace craftersmine.Synx.Server.Core
             predefinedConfig.Add("require-auth", "true");
             predefinedConfig.Add("allow-anonymous", "false");
 
-            try
+            log("info", "Initiating server environment...");
+            StaticData.UserStorage = Path.Combine(serverRoot, "user-storage");
+            string serverPropsFile = Path.Combine(serverRoot, "server-properties.cfg");
+            string banlistFile = Path.Combine(serverRoot, "banned-accounts.lst");
+            string accountsDataFile = Path.Combine(serverRoot, "accounts-data.lst");
+            if (!Directory.Exists(StaticData.UserStorage))
+                Directory.CreateDirectory(StaticData.UserStorage);
+            if (!File.Exists(serverPropsFile))
             {
-                log("info", "Initiating server environment...");
-                StaticData.UserStorage = Path.Combine(serverRoot, "user-storage");
-                string serverPropsFile = Path.Combine(serverRoot, "server-properties.cfg");
-                string banlistFile = Path.Combine(serverRoot, "banned-accounts.lst");
-                string accountsDataFile = Path.Combine(serverRoot, "accounts-data.lst");
-                if (!Directory.Exists(StaticData.UserStorage))
-                    Directory.CreateDirectory(StaticData.UserStorage);
-                if (!File.Exists(serverPropsFile))
+                List<string> cfg = new List<string>();
+                foreach (var predefCfg in predefinedConfig)
+                    cfg.Add(string.Join("=", predefCfg.Key, predefCfg.Value));
+                File.WriteAllLines(serverPropsFile, cfg);
+            }
+            else
+            {
+                List<string> lines = new List<string>();
+                lines.AddRange(File.ReadAllLines(serverPropsFile));
+                List<string> toWrite = new List<string>();
+                List<string> keys = new List<string>();
+                foreach (var prop in lines)
+                    keys.Add(prop.Split('=')[0]);
+                foreach (var key in predefinedConfig)
                 {
-                    List<string> cfg = new List<string>();
-                    foreach (var predefCfg in predefinedConfig)
-                        cfg.Add(string.Join("=", predefCfg.Key, predefCfg.Value));
-                    File.WriteAllLines(serverPropsFile, cfg);
-                }
-                else
-                {
-                    List<string> lines = new List<string>();
-                    lines.AddRange(File.ReadAllLines(serverPropsFile));
-                    List<string> toWrite = new List<string>();
-                    List<string> keys = new List<string>();
-                    foreach (var prop in lines)
-                        keys.Add(prop.Split('=')[0]);
-                    foreach (var key in predefinedConfig)
+                    if (!keys.Contains(key.Key))
                     {
-                        if (!keys.Contains(key.Key))
-                        {
-                            lines.Add(string.Join("=", key.Key, key.Value));
-                        }
-                    }
-                    if (lines.Any())
-                    {
-                        File.WriteAllLines(serverPropsFile, lines);
+                        lines.Add(string.Join("=", key.Key, key.Value));
                     }
                 }
-                if (!File.Exists(banlistFile))
-                    File.WriteAllText(banlistFile, "");
-                if (!File.Exists(accountsDataFile))
-                    File.WriteAllText(accountsDataFile, "");
-                log("info", "Server environment initiated!");
+                if (lines.Any())
+                {
+                    File.WriteAllLines(serverPropsFile, lines);
+                }
             }
-            catch (Exception ex)
-            {
-                logException("error", ex);
-            }
-            finally
-            {
-                Environment.Exit(0);
-            }
+            if (!File.Exists(banlistFile))
+                File.WriteAllText(banlistFile, "");
+            if (!File.Exists(accountsDataFile))
+                File.WriteAllText(accountsDataFile, "");
+            log("info", "Server environment initiated!");
         }
 
         private static void log(string prefix, string contents)
         {
             StaticData.LoggerInstance.Log(prefix, contents);
-        }
-
-        private static void logException(string prefix, Exception ex)
-        {
-            StaticData.LoggerInstance.LogException(prefix, ex);
         }
     }
 }
