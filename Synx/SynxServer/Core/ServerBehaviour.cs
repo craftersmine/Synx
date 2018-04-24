467c8f0ede0a8ff16e5519f8bc6454881dc37489
Rodion <rodion.morozov.01@gmail.com>
2018-04-24 23:58:19 +0700

    Added Server Behaviour Dummy file

---
+++ Synx/SynxServer/Core/ServerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.Packager.Lib.Core;
+using craftersmine.Packager.Lib.Core.Exceptions;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace craftersmine.Synx.Server.Core
+{
+    public sealed class ServerBehaviour : WebSocketBehavior
+    {
+    }
+}
