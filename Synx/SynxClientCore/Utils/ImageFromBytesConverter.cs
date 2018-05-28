66346b01a1f17aa2177b594216eefafb1ab97709
Rodion <rodion.morozov.01@gmail.com>
2018-05-29 01:07:49 +0700

    Added Image from Bytes converter

---
+++ Synx/SynxClientCore/Utils/ImageFromBytesConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace craftersmine.Synx.Client.Utils
+{
+    public sealed class ImageFromBytesConverter
+    {
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            Image returnImage = null;
+            if (byteArrayIn != null)
+            {
+                MemoryStream ms = new MemoryStream(byteArrayIn);
+                ms.Position = 0;
+                returnImage = Image.FromStream(ms);
+            }
+            return returnImage;
+        }
+    }
+}
