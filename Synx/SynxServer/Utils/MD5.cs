f0f02c4403f2cfb60847bec62f4da6431a4233c3
Rodion <rodion.morozov.01@gmail.com>
2018-05-29 19:29:30 +0700

    Added MD5 compute utility class

---
+++ Synx/SynxServer/Utils/MD5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace craftersmine.Synx.Server.Utils
+{
+    public sealed class MD5Compute
+    {
+        public static string ComputeMD5(string input)
+        {
+            MD5 _md5 = new MD5CryptoServiceProvider();
+
+            byte[] returned = _md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < returned.Length; i++)
+            {
+                sb.Append(returned[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
