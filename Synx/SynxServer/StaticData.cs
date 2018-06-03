1f813cece6ec3e2ca64ee61a5e963cc11b102698
Rodion <rodion.morozov.01@gmail.com>
2018-06-03 17:48:28 +0700

    Added paths storages

--- Synx/SynxServer/StaticData.cs
+++ Synx/SynxServer/StaticData.cs
@@ -7,6 +7,7 @@ using WebSocketSharp.Server;
 using WebSocketSharp;
 using craftersmine.Packager.Lib.Core;
 using craftersmine.Packager.Lib.Core.Exceptions;
+using craftersmine.Synx.Server.Core;
 
 namespace craftersmine.Synx.Server
 {
@@ -14,10 +15,14 @@ namespace craftersmine.Synx.Server
     {
         public static WebSocketServer ServerInstance { get; set; }
         public static Utils.Logger LoggerInstance { get; set; }
+        public static UserStorage UserStorage { get; set; }
+        public static CommandParser CommandParser { get; set; }
 
-        public static string UserStorage { get; set; }
+        public static string UserStorageFolderPath { get; set; }
         public static string LogsPath { get; set; }
         public static string ServerRoot { get; set; }
+        public static string AccountsFilePath { get; set; }
+        public static string BannedFilePath { get; set; }
 
         public static bool IsServerRunning { get; set; }
     }
